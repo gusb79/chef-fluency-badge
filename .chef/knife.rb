cb558ef30449ae07356eab93d697629dbb6182d1
Gustavo Benitez <gusb79@gmail.com>
2018-02-03 12:44:50 +0000

    Adding Chef Repo to GIT

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "gus"
+client_key               "#{current_dir}/gus.pem"
+chef_server_url          "https://gustavo-romerobenitez-9f5096091.mylabserver.com/organizations/gusorg"
+cookbook_path            ["#{current_dir}/../cookbooks"]
