f2bb92904969a86888f9888ff6156eb6ec1aceee
Gustavo Benitez <gusb79@gmail.com>
2018-02-04 00:03:14 +0000

    Added motd recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,8 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+f node['platform_family'] == 'rhel'
+	package = 'httpd'
+elseif node['platform_family'] == 'debian'
+	package = 'apache2'
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
